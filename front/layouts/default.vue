5325e35818451af1a0a9e2b40a6bcd634b324277
mikael briolet <mbriolet.ma@gmail.com>
2020-08-22 21:44:42 +0200

    💄 Remove nuxt menu - add a very simple menu

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,38 +1,20 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
+    <b-navbar type="is-dark">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          Les Fontaines
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="/workers/">
+          Workers
+        </b-navbar-item>
+      </template>
+    </b-navbar>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
+    <section class="main-content">
+      <div class="container is-10">
         <nuxt />
       </div>
     </section>
