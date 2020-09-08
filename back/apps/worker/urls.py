317ca5a879d2ea710770bf77c027411f24e879d4
mikael <mbriolet.ma@gmail.com>
2020-09-08 13:56:45 +0200

    ✨ add url converter for negative integer

--- back/apps/worker/urls.py
+++ back/apps/worker/urls.py
@@ -1,10 +1,27 @@
 """Worker urls."""
 
-from django.urls import path, include
+from django.urls import path, include, register_converter
 
 from rest_framework.routers import DefaultRouter
 
 from . import views
+
+
+class NegativeIntConverter:
+    """URL converter - catch negative integers."""
+
+    regex = r"-?\d+"
+
+    def to_python(self, value):
+        """To python."""
+        return int(value)
+
+    def to_url(self, value):
+        """To url."""
+        return "%d" % value
+
+
+register_converter(NegativeIntConverter, "negint")
 
 
 router = DefaultRouter()
@@ -13,5 +30,5 @@ router.register("workday", views.WorkDayViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("month/<int:from_now>/", views.get_month, name="get_month"),
+    path("month/<negint:from_now>/", views.get_month, name="get_month"),
 ]
