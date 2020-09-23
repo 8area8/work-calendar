0acdbfc376ab1009f862e872da8108102674d8d7
mikael briolet <mbriolet.ma@gmail.com>
2020-09-23 12:40:31 +0200

    ✨ authenticate if press enter key

--- front/src/views/Auth.vue
+++ front/src/views/Auth.vue
@@ -18,7 +18,7 @@
           Le nom d'utilisateur et/ou le mot de passe est invalide.
         </b-message>
 
-        <form :action="authenticate">
+        <form v-on:keyup.enter="authenticate">
           <b-field label="Nom d'utilisateur">
             <b-input id="username" v-model="username"></b-input>
           </b-field>
@@ -28,6 +28,7 @@
           <b-button
             id="authenticate"
             type="is-warning"
+            value="Submit"
             expanded
             @click="authenticate"
           >
