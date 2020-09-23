485f007af24b81e69233334655e1b19456223716
mikael briolet <mbriolet.ma@gmail.com>
2020-09-23 15:26:56 +0200

    ✨ django admin - add objects to admin panel

--- back/apps/worker/admin.py
+++ back/apps/worker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Day, WorkDay, Employee
 
-# Register your models here.
+
+class DayAdmin(admin.ModelAdmin):
+    pass
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkDayAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Day, DayAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(WorkDay, WorkDayAdmin)
