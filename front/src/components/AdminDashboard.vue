76315c265a3c4a9d57b0bcb9ffe4b27e413f2b8e
mikael briolet <mbriolet.ma@gmail.com>
2020-09-27 19:13:29 +0200

    ✨ add admin dashboard week table
    + lot of boilerplate

--- front/src/components/AdminDashboard.vue
+++ front/src/components/AdminDashboard.vue
@@ -2,43 +2,84 @@
   <div class="container has-text-centered box" style="margin-top: 1em">
     <div class="table-container">
       <div class="dashboard__title">Tableau de bord</div>
-      <table
-        v-if="filter === -1"
-        class="table is-bordered is-striped"
-        style="margin: auto; margin-top: 1em"
-      >
-        <thead>
-          <tr>
-            <th>Employé</th>
-            <th><abbr>Jours travaillés</abbr></th>
-            <th><abbr>Total heures</abbr></th>
-            <th><abbr>Salaire net à l'heure</abbr></th>
-            <th><abbr>Salaire au mois</abbr></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="employee in employees"
-            :key="'month-salary-' + employee.id"
-          >
-            <td style="vertical-align: middle;">
-              {{ employee.name }}
-            </td>
-            <td style="vertical-align: middle;">
-              {{ getEmployeeWorks(employee.id).length }}
-            </td>
-            <td style="vertical-align: middle;">
-              {{ getTime(employee) }}
-            </td>
-            <td style="vertical-align: middle;">
-              {{ employee.salary }}
-            </td>
-            <td style="vertical-align: middle;">
-              {{ getMonthSalary(employee) }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-if="filter === -1">
+        <div class="title is-size-5">Salaires au mois</div>
+        <table
+          class="table is-bordered is-striped"
+          style="margin: auto; margin-top: 1em"
+        >
+          <thead>
+            <tr>
+              <th>Employé</th>
+              <th><abbr>Jours travaillés</abbr></th>
+              <th><abbr>Total heures</abbr></th>
+              <th><abbr>Salaire net à l'heure</abbr></th>
+              <th><abbr>Salaire au mois</abbr></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="employee in employees"
+              :key="'month-salary-' + employee.id"
+            >
+              <td style="vertical-align: middle;">
+                {{ employee.name }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ getEmployeeWorks(employee.id).length }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ getTime(employee) }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ employee.salary }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ getMonthSalary(employee) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        <div class="title is-size-5">
+          Salaire de {{ employees.find((elem) => elem.id == filter).name }} à la
+          semaine (salaire / heure :
+          {{ employees.find((elem) => elem.id == filter).salary }} euros)
+        </div>
+        <table
+          class="table is-bordered is-striped"
+          style="margin: auto; margin-top: 1em"
+        >
+          <thead>
+            <tr>
+              <th>Numéro de semaine</th>
+              <th><abbr>Jours travaillés</abbr></th>
+              <th><abbr>Total heures</abbr></th>
+              <th><abbr>Salaire à la semaine</abbr></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(week, index) in getWeeks()"
+              :key="'week-salary-' + index"
+            >
+              <td style="vertical-align: middle;">
+                {{ index + 1 }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ week.length }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ getWeekTime(week) }}
+              </td>
+              <td style="vertical-align: middle;">
+                {{ getWeekSalary(week) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -114,8 +155,58 @@ export default class Dashboard extends Props {
     return salary.toFixed(2);
   }
 
-  mounted() {
-    // this.getMonthWorks();
+  getWeeks(): IWorkDate[][] {
+    const month = this.calendar.getMonth();
+    const weeks: IWorkDate[][] = [];
+
+    this.calendar.days.forEach((day: IDay, index: number) => {
+      if (index % 7 == 0) {
+        weeks.push([]);
+      }
+      if (day.month === month) {
+        const work = day.works.find((work: IWorkDate) => {
+          return work.employee === this.filter;
+        });
+        if (work) {
+          weeks.slice(-1)[0].push(work);
+        }
+      }
+    });
+    return weeks;
+  }
+
+  getWeekHours(week: IWorkDate[]) {
+    let seconds = 0;
+    let hours = 0;
+    let minutes = 0;
+
+    for (const work of week) {
+      seconds = (work.end.getTime() - work.start.getTime()) / 1000;
+      minutes += seconds / 60;
+    }
+    hours = Math.floor(minutes / 60);
+    minutes -= Math.floor(hours * 60);
+
+    return { hours, minutes };
+  }
+
+  getWeekTime(week: IWorkDate[]): string {
+    const time = this.getWeekHours(week);
+    const twoDigits = time.minutes < 10 ? "0" + time.minutes : time.minutes;
+    return `${time.hours}H${twoDigits}`;
+  }
+
+  getWeekSalary(week: IWorkDate[]): string {
+    let salary = 0;
+    const employee = this.employees.find(
+      (employee: ISalaryWorker) => employee.id == this.filter
+    );
+    const baseNet = employee ? employee.salary : 0;
+    const time = this.getWeekHours(week);
+
+    salary = baseNet * time.hours;
+    salary += (baseNet * time.minutes) / 60;
+    return salary.toFixed(2);
   }
 }
 </script>
@@ -124,5 +215,6 @@ export default class Dashboard extends Props {
 .dashboard__title {
   font-size: 3em;
   font-family: "Berkshire Swash", cursive;
+  margin-bottom: 1em;
 }
 </style>
